4a8bd15756445f4a1af543f63c83087e04e917ef
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-18 17:55:07 -0400

    Finish combining string array values challenge

---
+++ Section4-VariableData/combineStringArrayChallenge/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+string letters = "";
+decimal sum = 0;
+decimal result = 0;
+
+for (int i = 0; i < values.Length; i ++)
+{
+    if (decimal.TryParse(values[i], out result))
+        sum += result;
+    else 
+        letters += values[i];
+}   
+
+Console.WriteLine($"Message: {letters}");
+Console.WriteLine($"Total: {sum}");
