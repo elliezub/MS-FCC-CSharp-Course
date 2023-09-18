707007a3486c86a792ae496d346f07a0f1166cbb
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-18 19:18:51 -0400

    Compete parse string challenge

--- Section4-VariableData/parseStringChallenge/Program.cs
+++ Section4-VariableData/parseStringChallenge/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orderArray = orderStream.Split(",");
+Array.Sort(orderArray);
+
+foreach (string order in orderArray)
+{
+    if (order.Length == 4)
+        Console.WriteLine(order);
+    else
+        Console.WriteLine($"{order}\t- Error");
+}
