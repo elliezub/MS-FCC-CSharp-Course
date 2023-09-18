294220424a26eae3efd4b9380d6d3a6fdb04657f
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-18 17:32:45 -0400

    Finish data type practice

--- Section4-VariableData/dataTypePractice/Program.cs
+++ Section4-VariableData/dataTypePractice/Program.cs
@@ -42,6 +42,20 @@
 // int value2 = Convert.ToInt32(1.5m); // converting rounds up 
 // Console.WriteLine(value2);
 
-string name = "Bob";
-Console.WriteLine(int.Parse(name));
+// string name = "Bob";
+// Console.WriteLine(int.Parse(name));
+
+string value = "bad";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else 
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
+
+if (result > 0)
+    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 
