3c5976aded2905bf2d81b9073158364ad18507ae
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-20 13:19:18 -0400

    Completed string interpolation practice

--- Section4-VariableData/stringPractice/Program.cs
+++ Section4-VariableData/stringPractice/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"      Sub Total: {subtotal:C}");
+Console.WriteLine($"            Tax: {taxPercentage:P2}");
+Console.WriteLine($"      Total Billed: {total:C}");
+
