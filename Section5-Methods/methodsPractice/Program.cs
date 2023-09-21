f37731fb499dbf3b02b464058ac84fc2cf1af1ab
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-21 17:28:34 -0400

    Complete first methods practice

--- Section5-Methods/methodsPractice/Program.cs
+++ Section5-Methods/methodsPractice/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Generating random numbers:");
+DisplayRandomNumbers();
+
+void DisplayRandomNumbers()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write($"{random.Next(1, 100)} ");
+    }
+    Console.WriteLine();
+}
