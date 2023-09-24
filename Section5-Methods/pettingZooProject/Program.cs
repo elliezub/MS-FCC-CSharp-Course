e8a98d868d6578a30925415699a940e7129071cb
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-24 15:36:05 -0400

    Created method to shuffle animal array

--- Section5-Methods/pettingZooProject/Program.cs
+++ Section5-Methods/pettingZooProject/Program.cs
@@ -7,10 +7,21 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-// RandomizeAnimals();
-
+RandomizeAnimals();
 // string[,] group = AssignGroup();
-
 Console.WriteLine("School A");
-
 //PrintGroup(group);
+
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++)
+    {
+    int r = random.Next(i, pettingZoo.Length);
+
+    string temp = pettingZoo[i];
+    pettingZoo[i] = pettingZoo[r];
+    pettingZoo[r] = temp;
+    }
+}
