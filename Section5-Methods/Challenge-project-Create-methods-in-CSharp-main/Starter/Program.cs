6a8815b53e31cfd3bfab55e51d71e9752288910a
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-24 17:09:13 -0400

    Add logic so player changes when it eats food

--- Section5-Methods/Challenge-project-Create-methods-in-CSharp-main/Starter/Program.cs
+++ Section5-Methods/Challenge-project-Create-methods-in-CSharp-main/Starter/Program.cs
@@ -15,8 +15,8 @@ int foodX = 0;
 int foodY = 0;
 
 // Available player and food strings
-string[] states = {"('-')", "(^-^)", "(X_X)"};
-string[] foods = {"@@@@@", "$$$$$", "#####"};
+string[] states = { "('-')", "(^-^)", "(X_X)" };
+string[] foods = { "@@@@@", "$$$$$", "#####" };
 
 // Current player string displayed in the Console
 string player = states[0];
@@ -24,20 +24,36 @@ string player = states[0];
 // Index of the current food
 int food = 0;
 
+
 InitializeGame();
-while (!shouldExit) 
+while (!shouldExit)
 {
-    Move();
+    if (TerminalResized())
+    {
+        Console.Clear();
+        Console.WriteLine("Console was resized. Program Exiting.");
+        shouldExit = true;
+    }
+    else
+    {
+        Move();
+        if (NeedFood())
+        {
+            ChangePlayer();
+            ShowFood();
+        }
+    }
+        
 }
 
 // Returns true if the Terminal was resized 
-bool TerminalResized() 
+bool TerminalResized()
 {
     return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
 }
 
 // Displays random food at a random location
-void ShowFood() 
+void ShowFood()
 {
     // Update food to a random index
     food = random.Next(0, foods.Length);
@@ -51,8 +67,13 @@ void ShowFood()
     Console.Write(foods[food]);
 }
 
+bool NeedFood()
+{
+    return foodX == playerX && foodY == playerY;
+}
+
 // Changes the player to match the food consumed
-void ChangePlayer() 
+void ChangePlayer()
 {
     player = states[food];
     Console.SetCursorPosition(playerX, playerY);
@@ -60,40 +81,55 @@ void ChangePlayer()
 }
 
 // Temporarily stops the player from moving
-void FreezePlayer() 
+void FreezePlayer()
 {
     System.Threading.Thread.Sleep(1000);
     player = states[0];
 }
 
 // Reads directional input from the Console and moves the player
-void Move() 
+void Move(bool otherKeysExit = true)
 {
     int lastX = playerX;
     int lastY = playerY;
-    
-    switch (Console.ReadKey(true).Key) 
+
+    switch (Console.ReadKey(true).Key)
     {
         case ConsoleKey.UpArrow:
-            playerY--; 
+            playerY--;
             break;
-		case ConsoleKey.DownArrow: 
-            playerY++; 
+        case ConsoleKey.DownArrow:
+            playerY++;
             break;
-		case ConsoleKey.LeftArrow:  
-            playerX--; 
+        case ConsoleKey.LeftArrow:
+            playerX--;
             break;
-		case ConsoleKey.RightArrow: 
-            playerX++; 
+        case ConsoleKey.RightArrow:
+            playerX++;
             break;
-		case ConsoleKey.Escape:     
-            shouldExit = true; 
+        case ConsoleKey.Escape:
+            shouldExit = true;
+            if (shouldExit)
+            {
+                Console.Clear();
+                Console.WriteLine("You clicked the escape key. Program exiting.");
+                return;
+            }
+            break;
+        default:
+            shouldExit = otherKeysExit;
+            if (shouldExit)
+            {
+                Console.Clear();
+                Console.WriteLine("You clicked a nondirectional key. Program exiting.");
+                return;
+            }
             break;
     }
 
     // Clear the characters at the previous position
     Console.SetCursorPosition(lastX, lastY);
-    for (int i = 0; i < player.Length; i++) 
+    for (int i = 0; i < player.Length; i++)
     {
         Console.Write(" ");
     }
@@ -108,7 +144,7 @@ void Move()
 }
 
 // Clears the console, displays the food and player
-void InitializeGame() 
+void InitializeGame()
 {
     Console.Clear();
     ShowFood();
