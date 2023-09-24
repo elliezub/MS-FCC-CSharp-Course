863a3532c62618d11eb9833bdf04587fee7aaf32
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-24 15:41:30 -0400

    Created assign group method

--- Section5-Methods/pettingZooProject/Program.cs
+++ Section5-Methods/pettingZooProject/Program.cs
@@ -8,7 +8,7 @@ string[] pettingZoo =
 };
 
 RandomizeAnimals();
-// string[,] group = AssignGroup();
+string[,] group = AssignGroup();
 Console.WriteLine("School A");
 //PrintGroup(group);
 
@@ -24,4 +24,19 @@ void RandomizeAnimals()
     pettingZoo[i] = pettingZoo[r];
     pettingZoo[r] = temp;
     }
+}
+
+string[,] AssignGroup(int groups = 6)
+{
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
+    
+    for (int i = 0; i < groups; i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+    return result;
 }
