0f74b972ec9ff6e28c65006baf2cf47de1edc3de
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-26 16:28:13 -0400

    complete catch separate exception types practice

--- Section6-Debugging/tryCatchPractice/Program.cs
+++ Section6-Debugging/tryCatchPractice/Program.cs
@@ -46,36 +46,67 @@
 
 //trycatch challenge
 
-try
-{
-    Process1();
-}
-catch
-{
-    Console.WriteLine("An exception has occurred");
-}
+// try
+// {
+//     Process1();
+// }
+// catch
+// {
+//     Console.WriteLine("An exception has occurred");
+// }
 
-Console.WriteLine("Exit program");
+// Console.WriteLine("Exit program");
 
-static void Process1()
+// static void Process1()
+// {
+//     try
+//     {
+//         WriteMessage();
+//     }
+//     catch (DivideByZeroException ex)
+//     {
+//         Console.WriteLine($"exception caught in Process1: {ex.Message}");
+//     }
+// }
+
+// static void WriteMessage()
+// {
+//     double float1 = 3000.0;
+//     double float2 = 0.0;
+//     int number1 = 3000;
+//     int number2 = 0;
+//     byte smallNumber;
+
+//     Console.WriteLine(float1 / float2);
+//     Console.WriteLine(number1 / number2);
+
+//     checked
+//     {
+//         smallNumber = (byte)number1;
+//     }
+// }
+
+// catch separate exception types in a code block
+
+string[] inputValues = new string[]{"three", "9999999999", "0", "2"};
+
+foreach (string inputValue in inputValues)
 {
+    int numValue = 0;
     try
     {
-        WriteMessage();
+        numValue = int.Parse(inputValue);
     }
-    catch
+    catch (FormatException)
     {
-        Console.WriteLine("exception caught in process1");
+        Console.WriteLine("Invalid readResult. Please enter a valid number");
     }
-}
-
-static void WriteMessage()
-{
-    double float1 = 3000.0;
-    double float2 = 0.0;
-    int number1 = 3000;
-    int number2 = 0;
-
-    Console.WriteLine(float1 / float2);
-    Console.WriteLine(number1 / number2);
+    catch (OverflowException)
+    {
+        Console.WriteLine("The number you entered is too large or too small.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
 }
